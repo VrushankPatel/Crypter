6cc7f6263abf5624e9dfb71429f86f5fefb295d0
Vrushank Patel <34762923+VrushankPatel@users.noreply.github.com>
2020-06-03 21:08:46 +0530

    Added RC4

--- EncryptionEngine/EncryptionEngine.py
+++ EncryptionEngine/EncryptionEngine.py
@@ -1,5 +1,6 @@
 import hashlib 
 import sys
+import RC4
 
 class SHA:
     def __init__(self,str):
@@ -23,13 +24,21 @@ class SHA:
     def md5(self):
         return hashlib.md5(self.str.encode()).hexdigest()        
 
-    def getEncrypted(self,object,encryption):
-        return getattr(object,encryption)()
+    def rc4(self,key):
+        print(key)
+        return RC4.rc4crypt(self.str,key)
+
+    def getEncrypted(self,object,encryption,param):
+        if param:
+            return getattr(object,encryption)(param)
+        else:
+            return getattr(object,encryption)()
 
 sha = SHA("GeeksforGeeks")
-print(sha.getEncrypted(sha,"sha256"))
-print(sha.getEncrypted(sha,"sha384"))
-print(sha.getEncrypted(sha,"sha224"))
-print(sha.getEncrypted(sha,"sha512"))
-print(sha.getEncrypted(sha,"sha1"))
-print(sha.getEncrypted(sha,"md5"))
+print(sha.getEncrypted(sha,"sha256",None))
+print(sha.getEncrypted(sha,"sha384",None))
+print(sha.getEncrypted(sha,"sha224",None))
+print(sha.getEncrypted(sha,"sha512",None))
+print(sha.getEncrypted(sha,"sha1",None))
+print(sha.getEncrypted(sha,"md5",None))
+print(sha.getEncrypted(sha,"rc4","Vrushank"))
--- Main.py
+++ Main.py
@@ -1,10 +1,19 @@
 from GUI import Window
 
+options = [
+    "SHA256",
+    "SHA384",
+    "SHA224",
+    "SHA512",
+    "SHA1",
+    "MD5",
+    "RC4"
+]
 if __name__ == "__main__":
     window = Window("Crypter")        
     
     window.getLabel("Choose the Encryption Angorithm : ").grid(row=0,column=0)
-    window.getDropDownMenu(["Jan","Feb","Mar"]).grid(row=0,column=1)
+    window.getDropDownMenu(options).grid(row=0,column=1)
     window.getLabel("Enter String to be encrypted : ").grid(row=2,column=0)
     entry1 = window.getTextArea(3,16).grid(row=2,column=1)
     
